3d16e32ab4d0ff1745a4ececbdfeb267f0d561b5
Christopher Díaz <nealthmc@gmail.com>
2022-08-27 18:45:56 -0500

    better initial structure

--- pages/resume/index.tsx
+++ pages/resume/index.tsx
@@ -3,19 +3,22 @@ import cover from '../../images/hero_cover.jpg'
 
 const Resume: NextPage = () => {
     return (
-        <div className="min-h-screen">
-            <div
-                style={{
-                    // TODO: Remove hardcoded
-                    backgroundImage: `url(${cover.src})`,
-                }}
-                className="min-h-[inherit] bg-fixed bg-no-repeat bg-center bg-cover"
-            ></div>
-            <div
-                className="absolute top-0 left-0 w-full h-full bg-black opacity-60"
-            ></div>
+        <main>
+            <section className="min-h-screen relative">
+                <div
+                    style={{
+                        // TODO: Remove hardcoded
+                        backgroundImage: `url(${cover.src})`,
+                    }}
+                    className="min-h-[inherit] bg-fixed bg-cover bg-no-repeat bg-center absolute top-0 left-0 h-full w-full"
+                ></div>
+                <div
+                    className="absolute top-0 left-0 w-full h-full bg-black opacity-60"
+                ></div>
+            </section>
             <div className="w-full h-screen"></div>
-        </div>
+        </main>
+
     )
 }
 
