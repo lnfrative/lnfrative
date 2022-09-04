c79c51c0cfb47c76817ac27f9da1832b1c95c0c5
Christopher Díaz <nealthmc@gmail.com>
2022-09-04 05:16:33 -0500

    create initial nav

--- pages/resume/index.tsx
+++ pages/resume/index.tsx
@@ -1,9 +1,47 @@
 import type { NextPage } from 'next'
+import Image from 'next/image'
 import cover from '../../images/hero_cover.jpg'
+import logoLight from '../../images/logo_light.svg'
 
 const Resume: NextPage = () => {
     return (
         <main>
+            <header className="transition-all ease-in delay-500">
+                <nav className="absolute w-full z-30 text-white">
+                    <div className="flex items-center justify-between px-3">
+                        <div className="w-2/12">
+                            <Image src={logoLight}></Image>
+                        </div>
+
+                        <div className="w-8/12 flex items-center justify-center">
+                            <ul className="flex">
+                                <li>
+                                    <a className="h-[70px] p-5 flex justify-center items-center">Cover</a>
+                                </li>
+                                <li>
+                                    <a className="h-[70px] p-5 flex justify-center items-center">Personal</a>
+                                </li>
+                                <li>
+                                    <a className="h-[70px] p-5 flex justify-center items-center">Services</a>
+                                </li>
+                                <li>
+                                    <a className="h-[70px] p-5 flex justify-center items-center">Resume</a>
+                                </li>
+                                <li>
+                                    <a className="h-[70px] p-5 flex justify-center items-center">Portfolio</a>
+                                </li>
+                                <li>
+                                    <a className="h-[70px] p-5 flex justify-center items-center">Hire</a>
+                                </li>
+                            </ul>
+                        </div>
+
+                        <div className="w-2/12 flex justify-end">
+                            Icons
+                        </div>
+                    </div>
+                </nav>
+            </header>
             <section className="min-h-screen relative">
                 <div
                     style={{
@@ -18,7 +56,6 @@ const Resume: NextPage = () => {
             </section>
             <div className="w-full h-screen"></div>
         </main>
-
     )
 }
 
