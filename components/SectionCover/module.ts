299f9c74030cc46e0d0c1000643a33047bfe6966
Christopher Díaz <nealthmc@gmail.com>
2022-09-05 04:11:21 -0500

    sign correction for cover animation

--- components/SectionCover/module.ts
+++ components/SectionCover/module.ts
@@ -6,7 +6,7 @@ export function startScrollObserver(ref: RefObject<HTMLDivElement>) {
     scrollEvent.subscribe(() => {
         const { scrollY } = window
         if (scrollY < 1200 && ref.current) {
-            ref.current.style.backgroundPosition = `center -${window.scrollY / 2.3}px`
+            ref.current.style.backgroundPosition = `center ${-window.scrollY / 2.3}px`
         }
     })
 }
