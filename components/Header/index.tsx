680a73a7bd455ad515f8607db98def6c821b880e
Christopher Díaz <nealthmc@gmail.com>
2022-09-04 23:33:50 -0500

    header icons

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -3,6 +3,7 @@ import Image from "next/image"
 import logoLight from "../../images/logo_light.svg"
 import { useInView } from 'react-intersection-observer'
 import className from 'classnames'
+import { GitHub, Twitter, Facebook, YouTube } from '@mui/icons-material'
 
 function Header() {
     const { ref, inView } = useInView({
@@ -22,31 +23,52 @@ function Header() {
                         <Image src={logoLight}></Image>
                     </div>
 
-                    <div className="w-8/12 flex items-center justify-center">
+                    <div className="w-8/12 hidden items-center justify-center xl:flex">
                         <ul className="flex">
                             <li>
-                                <a href="#cover" className="h-[70px] p-5 flex justify-center items-center">Cover</a>
+                                <a href="#cover" className="p-5 flex justify-center items-center">Cover</a>
                             </li>
                             <li>
-                                <a href="#personal" className="h-[70px] p-5 flex justify-center items-center">Personal</a>
+                                <a href="#personal" className="p-5 flex justify-center items-center">Personal</a>
                             </li>
                             <li>
-                                <a href="#services" className="h-[70px] p-5 flex justify-center items-center">Services</a>
+                                <a href="#services" className="p-5 flex justify-center items-center">Services</a>
                             </li>
                             <li>
-                                <a href="#resume" className="h-[70px] p-5 flex justify-center items-center">Resume</a>
+                                <a href="#resume" className="p-5 flex justify-center items-center">Resume</a>
                             </li>
                             <li>
-                                <a href="#portfolio" className="h-[70px] p-5 flex justify-center items-center">Portfolio</a>
+                                <a href="#portfolio" className="p-5 flex justify-center items-center">Portfolio</a>
                             </li>
                             <li>
-                                <a href="#hire" className="h-[70px] p-5 flex justify-center items-center">Hire</a>
+                                <a href="#hire" className="p-5 flex justify-center items-center">Hire me</a>
                             </li>
                         </ul>
                     </div>
 
                     <div className="w-2/12 flex justify-end">
-                        Icons
+                        <ul className="flex">
+                            <li className="p-5">
+                                <a rel="noreferrer" target="_blank" href="https://github.com/lnfrative">
+                                    <GitHub />
+                                </a>
+                            </li>
+                            <li className="p-5">
+                                <a rel="noreferrer" target="_blank" href="https://twitter.com/infrative">
+                                    <Twitter />
+                                </a>
+                            </li>
+                            <li className="p-5">
+                                <a rel="noreferrer" target="_blank" href="https://facebook.com/infrative">
+                                    <Facebook />
+                                </a>
+                            </li>
+                            <li className="p-5">
+                                <a rel="noreferrer" target="_blank" href="https://www.youtube.com/channel/UCGCS2ZCnAKraBHda3UyGUCQ">
+                                    <YouTube />
+                                </a>
+                            </li>
+                        </ul>
                     </div>
                 </div>
             </nav>
