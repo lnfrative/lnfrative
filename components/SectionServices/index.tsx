0f9ea96f679bae4d078b1367cf65ad1e549b4021
Christopher Díaz <nealthmc@gmail.com>
2022-09-09 06:26:11 -0500

    temp serve resume as linktree

--- components/SectionServices/index.tsx
+++ components/SectionServices/index.tsx
@@ -1,12 +1,65 @@
 import React from "react"
 import Section from "../Section"
 import TitleOverlay from "../TitleOverlay"
+import GroupDescription from "../GroupDescription"
+
+import { Web } from "@mui/icons-material"
 
 function SectionServices() {
     return (
         <div className="bg-neutral-light dark:bg-neutral-dark">
             <Section index={2} id="services">
                 <TitleOverlay primary="What I do?" secondary="Services" />
+
+                <div className="flex mt-20">
+                    <div className="flex-1 mr-20">
+                        <GroupDescription
+                            title="Frontend Development"
+                            description="Building and maintaining scalable web applications using development frameworks with a strong focus on components as pieces of constituent code."
+                            imageElement={(
+                                <div className="text-primary-light dark:text-primary-dark text-5xl">
+                                    <Web color="inherit" fontSize="inherit" />
+                                </div>
+                            )}
+                        />
+
+                        <div className="mt-20" />
+
+                        <GroupDescription
+                            title="Frontend Development"
+                            description="Building and maintaining scalable web applications using development frameworks with a strong focus on components as pieces of constituent code."
+                            imageElement={(
+                                <div className="text-primary-light dark:text-primary-dark text-5xl">
+                                    <Web color="inherit" fontSize="inherit" />
+                                </div>
+                            )}
+                        />
+                    </div>
+
+                    <div className="flex-1">
+                        <GroupDescription
+                            title="Frontend Development"
+                            description="Building and maintaining scalable web applications using development frameworks with a strong focus on components as pieces of constituent code."
+                            imageElement={(
+                                <div className="text-primary-light dark:text-primary-dark text-5xl">
+                                    <Web color="inherit" fontSize="inherit" />
+                                </div>
+                            )}
+                        />
+
+                        <div className="mt-20" />
+
+                        <GroupDescription
+                            title="Frontend Development"
+                            description="Building and maintaining scalable web applications using development frameworks with a strong focus on components as pieces of constituent code."
+                            imageElement={(
+                                <div className="text-primary-light dark:text-primary-dark text-5xl">
+                                    <Web color="inherit" fontSize="inherit" />
+                                </div>
+                            )}
+                        />
+                    </div>
+                </div>
             </Section>
         </div>
     )
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,28 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 
+// page
+import Resume from "./resume"
+
 // components
 import Bio from "../components/Bio"
 import Linktree from "../components/Linktree"
 
 import styles from '../styles/Home.module.css'
 
-const Home: NextPage = () => {
-  return (
-    <div className={styles.container}>
-        <Head>
-            <title>Infrative | Linktree</title>
-            <meta name="description" content="Computer science student and software developer." />
-            <link rel="shortcut icon" type="image/svg+xml" href="/favicon.svg" />
-        </Head>
-        <Bio />
-        <Linktree />
-    </div>
-  )
-}
+// TODO: Redesign linktree.
+// const Home: NextPage = () => {
+//   return (
+//     <div className={styles.container}>
+//         <Head>
+//             <title>Infrative | Linktree</title>
+//             <meta name="description" content="Computer science student and software developer." />
+//             <link rel="shortcut icon" type="image/svg+xml" href="/favicon.svg" />
+//         </Head>
+//         <Bio />
+//         <Linktree />
+//     </div>
+//   )
+// }
 
-export default Home
+export default Resume
