12e057eb47927aa165441afb2ebcc15340202129
Christopher Díaz <nealthmc@gmail.com>
2022-09-14 08:24:26 -0500

    update favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,13 +3,13 @@ import type { AppProps } from "next/app"
 import { Provider } from "react-redux"
 import { store } from "../store"
 import Head from "next/head"
-import icon from "../images/icon.png"
+import icon from "../images/icon.svg"
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
       <Provider store={store}>
           <Head>
-              <link rel="icon" type="image/png" href={icon.src} />
+              <link rel="icon" type="image/svg+xml" href={icon.src} />
           </Head>
           <Component {...pageProps} />
       </Provider>
