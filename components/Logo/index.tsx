7ba998490f7c4a6dd6f2591deb8811c470cd27c1
Christopher Díaz <nealthmc@gmail.com>
2022-11-30 22:42:14 -0500

    Revert "update avatar"
    This reverts commit 9de21a19ffb36e0edbfa4ddcf00d068edbbb1975.

--- components/Logo/index.tsx
+++ components/Logo/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Image from 'next/image'
-import avatar from '../../images/avatar_navidad.jpg'
+import avatar from '../../images/avatar.jpg'
 
 function Logo() {
     return (
